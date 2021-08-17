a1a6b2de61dc3324ba905002bd1bf1cc93edf56a
Kornelia Chilczuk Kudan <kornelia.chilczuk@gmail.com>
2021-08-17 17:40:07 +0200

    Initial commit

---
+++ src/main/java/pl/javastart/lista/ListaApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.lista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/lista/ListaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.lista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
